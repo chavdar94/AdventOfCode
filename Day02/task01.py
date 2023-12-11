c9b35e1166f94b0d3e2fcf883bfcddf10be840ec
Chavdar <chokobg@gmail.com>
2023-12-11 04:32:52 +0200

    Better solution for d2t1

--- Day02/task01.py
+++ Day02/task01.py
@@ -1,7 +1,8 @@
-w_red = 12
-w_green = 13
-w_blue = 14
-
+winnings = {
+    'red': 12,
+    'green': 13,
+    'blue': 14
+}
 sum = 0
 
 file = 'puzzle.txt'
@@ -10,23 +11,21 @@ with open(file) as f:
         game = line.strip().split(':')
         cubes = game[1].split(';')
         game_id = int(game[0].strip().split()[1])
-        red = 0
-        green = 0
-        blue = 0
+        c_game = {
+            'red': 0,
+            'green': 0,
+            'blue': 0
+        }
 
         for move in cubes:
             pieces = move.strip().split(',')
             for piece in pieces:
                 count = int(piece.strip().split()[0])
                 color = piece.strip().split()[1]
-                if color == 'red' and count > red:
-                    red = count
-                if color == 'green' and count > green:
-                    green = count
-                if color == 'blue' and count > blue:
-                    blue = count
+                if count > c_game[color]:
+                    c_game[color] = count
 
-        if red <= w_red and green <= w_green and blue <= w_blue:
+        if all(c_game[color] <= winnings[color] for color in winnings):
             sum += game_id
 
 print(sum)
