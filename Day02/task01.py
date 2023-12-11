8914dc0e26ae5ba335d68962f9d6580bc7c686d4
Chavdar <chokobg@gmail.com>
2023-12-11 04:30:29 +0200

    day2 task01

--- Day02/task01.py
+++ Day02/task01.py
@@ -1,8 +1,7 @@
-winnings = {
-    'red': 12,
-    'green': 13,
-    'blue': 14
-}
+w_red = 12
+w_green = 13
+w_blue = 14
+
 sum = 0
 
 file = 'puzzle.txt'
@@ -11,21 +10,23 @@ with open(file) as f:
         game = line.strip().split(':')
         cubes = game[1].split(';')
         game_id = int(game[0].strip().split()[1])
-        c_game = {
-            'red': 0,
-            'green': 0,
-            'blue': 0
-        }
+        red = 0
+        green = 0
+        blue = 0
 
         for move in cubes:
             pieces = move.strip().split(',')
             for piece in pieces:
                 count = int(piece.strip().split()[0])
                 color = piece.strip().split()[1]
-                if count > winnings[color]:
-                    c_game[color] = count
+                if color == 'red' and count > red:
+                    red = count
+                if color == 'green' and count > green:
+                    green = count
+                if color == 'blue' and count > blue:
+                    blue = count
 
-        if all(c_game[color] < winnings[color] for color in winnings):
+        if red <= w_red and green <= w_green and blue <= w_blue:
             sum += game_id
 
 print(sum)
